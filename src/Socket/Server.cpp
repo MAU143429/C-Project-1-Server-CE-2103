874b051cc01e425034879177f4e801acf0ad710b
Naheem Johnson <60128330+NJohnsonCR@users.noreply.github.com>
2021-04-20 12:43:54 -0600

    Created class server

--- src/Socket/Server.cpp
+++ src/Socket/Server.cpp
@@ -1,5 +1,7 @@
 //
-// Created by mau14 on 19/4/21.
+// Created by njohnson on 4/19/21.
 //
 
 #include "Server.h"
+
+void Server::initServer() {}
--- src/Socket/Server.h
+++ src/Socket/Server.h
@@ -1,12 +1,22 @@
 //
-// Created by mau14 on 19/4/21.
+// Created by njohnson on 4/19/21.
 //
 
 #ifndef PROYECTO_1_DATOS_II_SERVER_C__SERVER_H
 #define PROYECTO_1_DATOS_II_SERVER_C__SERVER_H
-
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
 
 class Server {
+private:
+public:
+    static void initServer();
 
 };
 
